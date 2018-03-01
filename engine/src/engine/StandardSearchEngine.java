4de412cc855e734eacc0881f55cfc1dd940555eb
AndreaVidali <andreavidali@email.com>
2018-03-01 01:33:43 +0100

    engine working, missing GUI

--- engine/src/engine/StandardSearchEngine.java
+++ engine/src/engine/StandardSearchEngine.java
@@ -1,18 +1,14 @@
 package engine;
 
+// TODO rimuovere documenti usati per personalizzare utente
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
-import org.apache.commons.math3.linear.ArrayRealVector;
-import org.apache.commons.math3.linear.RealVector;
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-import org.apache.lucene.codecs.TermVectorsReader;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.*;
 import org.apache.lucene.queryparser.classic.ParseException;
@@ -23,7 +19,6 @@ import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.search.similarities.TFIDFSimilarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
-import org.apache.lucene.codecs.*;
 import org.apache.lucene.util.BytesRef;
 
 import static org.apache.lucene.index.IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS;
@@ -31,8 +26,6 @@ import static org.apache.lucene.index.IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_
 public class StandardSearchEngine {
 
     public static void main(String[] args) throws IOException, ParseException {
-
-        // http://www.lucenetutorial.com/lucene-in-5-minutes.html
 
         // Specify the analyzer for tokenizing text. The same analyzer should be used for indexing and searching
         StandardAnalyzer analyzer = new StandardAnalyzer();
@@ -63,66 +56,62 @@ public class StandardSearchEngine {
         buf.close();
         w.close();
 
-        // query
-        String querystr = args.length > 0 ? args[0] : "football";
-
-        // the "newsText" arg specifies the default field to use when no field is explicitly specified in the query.
-        Query q = new QueryParser("newsText", analyzer).parse(querystr);
-
-        // search
-        int hitsPerPage = 20;
+        // initialize reader and searcher
         IndexReader reader = DirectoryReader.open(index);
         IndexSearcher searcher = new IndexSearcher(reader);
         searcher.setSimilarity(similarity);
+
+        // user-document retrieval
+        String queryTopic = "tax";
+        Query qUser = new QueryParser("newsText", analyzer).parse(queryTopic);
+        int nResults = 3;
+        TopDocs docUser = searcher.search(qUser, nResults);
+        ScoreDoc[] hitsUser = docUser.scoreDocs;
+
+        System.out.println("--------- USER DOCS");
+        List<String> termsUserDoc = new ArrayList<>();
+        for (int i=0; i<hitsUser.length; ++i) {
+            int docUserId = hitsUser[i].doc;
+            Document dUser = searcher.doc(docUserId);
+            Terms docUserVector = reader.getTermVector(docUserId, "newsText");
+            TermsEnum itrUs = docUserVector.iterator();
+            BytesRef termUs;
+            while ((termUs = itrUs.next()) != null) {
+                String termTextUs = termUs.utf8ToString();
+                termsUserDoc.add(termTextUs);
+            }
+            System.out.println((i + 1) + ". " + "Score: " + hitsUser[i].score + " || " + dUser.get("createdAt") + "\t" + dUser.get("newsLink") + "\t" + dUser.get("tweet"));
+        }
+
+        // query
+        String querystr = "apple";
+        Query q = new QueryParser("newsText", analyzer).parse(querystr);
+
+        // search
+        int hitsPerPage = 200;
         TFIDFSimilarity tfidfSIM = new ClassicSimilarity();
         TopDocs docs = searcher.search(q, hitsPerPage);
         ScoreDoc[] hits = docs.scoreDocs;
+        System.out.println("---------- STANDARD SE");
         System.out.println("Found " + hits.length + " hits || Query: " + querystr);
 
-        // user-document retrieval
-        String queryTopic = "italy";
-        Query qUser = new QueryParser("newsText", analyzer).parse(queryTopic);
-        int nResults = 1;
-        TopDocs docUser = searcher.search(qUser, nResults);
-        ScoreDoc[] hitsUser = docUser.scoreDocs;
-        int docUserId = hitsUser[0].doc;
-        Terms docUserVector = reader.getTermVector(docUserId, "newsText");
-
-        Document dasd = searcher.doc(hitsUser[0].doc);
-
-        List<String> termsUserDoc = new ArrayList<>();
-        TermsEnum itrUs = docUserVector.iterator();
-        BytesRef termUs;
-        while ((termUs = itrUs.next()) != null) {
-            String termTextUs = termUs.utf8ToString();
-            termsUserDoc.add(termTextUs);
-        }
-
         // vector containing score for personalization
-        Map<Integer, Float> personalScoreContainer = new HashMap<>();
         Map<Integer, Double> combinedScoreContainer = new HashMap<>();
-        double alpha = 0.3;
+
+        // Personalization weight
+        double alpha = 0.20;
 
         // explore results
         for (int i=0; i<hits.length; ++i) {
 
-            // data structure to manage results document
-            HashMap<String,Integer> docWeights = new HashMap<>();
-
             int docId = hits[i].doc;
             Document d = searcher.doc(docId);
-
-            /*List<String> docText = getTokenizedText(d, "newsText");
-            System.out.println("Document text:"+docText);
-
-            Collections.sort(docText);
-            System.out.println("Document text sorted:"+docText);*/
 
             Terms docVector = reader.getTermVector(docId, "newsText");
             TermsEnum itr = docVector.iterator();
             BytesRef term;
 
-            float scorePersonal = 0;
+            ArrayList<Float> tfidfScores = new ArrayList<>();
 
             while ((term = itr.next()) != null) {
 
@@ -134,18 +123,23 @@ public class StandardSearchEngine {
                 searcher.search(q1, collector);
                 int docFreq = collector.getTotalHits();
 
-                // System.out.println("term: "+termText+" -- term frequency = "+termFreq+", document frequency = "+docFreq);
-
                 if(termsUserDoc.contains(termText)) {
                     float tf = tfidfSIM.tf(termFreq);
                     float idf = tfidfSIM.idf(docFreq, reader.numDocs());
-                    scorePersonal = scorePersonal + (tf * idf);
+                    tfidfScores.add(tf * idf);
                 }
             }
-
-            personalScoreContainer.put(docId, scorePersonal);
-
+            
             // combine results
+            Collections.sort(tfidfScores);
+            float scorePersonal = 0;
+            int remaining = 150;
+            for (int x = tfidfScores.size()-1; x>=0; --x) {
+                if(remaining != 0) {
+                    scorePersonal = scorePersonal + tfidfScores.get(x);
+                    remaining = remaining - 1;
+                }
+            }
             double combinedScore = alpha * scorePersonal + (1 - alpha) * hits[i].score;
             combinedScoreContainer.put(docId, combinedScore);
 
@@ -153,9 +147,7 @@ public class StandardSearchEngine {
 
         }
 
-        printSortedResults(combinedScoreContainer, hits, searcher);
-
-        // reader can only be closed when there is no need to access the documents any more.
+        printSortedResults(combinedScoreContainer, searcher, hits.length);
         reader.close();
     }
 
@@ -179,72 +171,44 @@ public class StandardSearchEngine {
         w.addDocument(doc);
     }
 
-    private static List<String> getTokenizedText(Document document, String field) throws IOException {
-        TokenStream stream = new StandardAnalyzer().tokenStream(field, new StringReader(document.getField(field).stringValue()));
-        List<String> result = new ArrayList<>();
-        try {
-            stream.reset();
-            while (stream.incrementToken()) {
-                result.add(stream.getAttribute(CharTermAttribute.class).toString());
-            }
-        } catch (IOException e) {
-            // not thrown b/c we're using a string reader...
-        }
-        return result;
-    }
+    private static void printSortedResults(Map<Integer, Double> combinedResults, IndexSearcher searcher, int nHits) {
 
-    private static float getScoreByDocID(ScoreDoc[] hits, int docID) {
-        for (ScoreDoc hit : hits) {
-            if (hit.doc == docID)
-                return hit.score;
-        }
-        return 0;
-    }
+        Collection valueSet = combinedResults.values();
+        Iterator iterator = valueSet.iterator();
+        ArrayList<Double> valueList = new ArrayList<>();
 
-    private static void printSortedResults(Map<Integer, Double> combinedResults, ScoreDoc[] hits, IndexSearcher searcher) {
-
-        Set<Integer> keySet = combinedResults.keySet();
-        Iterator iterator = keySet.iterator();
-
-        ArrayList<Integer> keyList = new ArrayList<>();
-
-        while (iterator.hasNext()){
-            //Integer key = iterator.next();
-            //keyList.add(key);
+        while (iterator.hasNext()) {
+            Object valueObj = iterator.next();
+            double value = (Double) valueObj;
+            valueList.add(value);
         }
 
-        Collections.sort(keyList);
+        Collections.sort(valueList);
 
-    }
+        System.out.println("--------- PERSONALIZED SE");
 
-    public LinkedHashMap<Integer, Double> sortHashMapByValues(HashMap<Integer, Double> passedMap) {
-        List<Integer> mapKeys = new ArrayList<>(passedMap.keySet());
-        List<String> mapValues = new ArrayList<>(passedMap.values());
-        Collections.sort(mapValues);
-        Collections.sort(mapKeys);
-
-        LinkedHashMap<Integer, String> sortedMap =
-                new LinkedHashMap<>();
-
-        Iterator<String> valueIt = mapValues.iterator();
-        while (valueIt.hasNext()) {
-            String val = valueIt.next();
-            Iterator<Integer> keyIt = mapKeys.iterator();
-
-            while (keyIt.hasNext()) {
-                Integer key = keyIt.next();
-                String comp1 = passedMap.get(key);
-                String comp2 = val;
-
-                if (comp1.equals(comp2)) {
-                    keyIt.remove();
-                    sortedMap.put(key, val);
-                    break;
-                }
+        for (int i = valueList.size()-1; i>=0; --i) {
+            Set docIdSet = getKeysByValue(combinedResults, valueList.get(i));
+            Object[] actualKey = docIdSet.toArray();
+            int docId = (Integer) actualKey[0];
+            try {
+                Document d = searcher.doc(docId);
+                System.out.println((nHits - i) + ". " + "Score (combined): " + valueList.get(i) + " || " + d.get("createdAt") + "\t" + d.get("newsLink") + "\t" + d.get("tweet"));
+            } catch (IOException e) {
+                System.out.println("____Missed document " + docId + " with result = " + valueList.get(i));
             }
         }
-        return sortedMap;
     }
+
+    private static <T, E> Set<T> getKeysByValue(Map<T, E> map, E value) {
+        return map.entrySet()
+                .stream()
+                .filter(entry -> Objects.equals(entry.getValue(), value))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+    }
+
+
 
 
 }
